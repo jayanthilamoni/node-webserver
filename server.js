52188b35f6c066fad5fe6523a2555015fb1904f5
Jayanth Ilamoni <jayanthilamoni@yahoo.in>
2017-11-22 18:44:31 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getYear',() => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+});
+app.set('view express','hbs');
+
+app.use((req, res, next) => {
+  console.log('Running ',req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', (request, response) => {
+  //response.send('Hello Express!');
+  response.render('home.hbs',{
+    pageTitle: 'Home Page Here',
+    welcomeMessage: 'Welcome to brand new Website'
+  });
+});
+
+app.get('/about',(request, response) => {
+  response.render('about.hbs',{
+    pageTitle: 'About Page Here'
+  });
+});
+
+app.get('/bad',(req, res) => {
+  res.send({
+    status: 400,
+    error: 'Page not found!'
+  });
+});
+
+app.listen(3000,() => {
+  console.log('Server is up at 3000');
+});
